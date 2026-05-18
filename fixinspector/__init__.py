f0a51d5862ca78b7bcf31d30f7ef1b9c70a0106c
Nelson Chan <khchanel@gmail.com>
2026-05-18 23:56:43 +1000

    chore: bump version for next release

--- fixinspector/__init__.py
+++ fixinspector/__init__.py
@@ -1,5 +1,13 @@
 """FIX Inspector package."""
 
-__all__ = ["__version__"]
+from importlib.metadata import version, PackageNotFoundError
 
-__version__ = "0.1.0"
+try:
+    # package name defined in [project.name] of pyproject.toml
+    __version__ = version("fixinspector")
+except PackageNotFoundError:
+    # fallback for when the package is not installed (e.g. during local dev)
+    __version__ = "0.0.0"
+
+
+__all__ = ["__version__"]
